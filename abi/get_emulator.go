1642857d725220eb9a3f347a1f48c499371ad34d
iam047801 <datbooi@tuta.io>
2023-04-30 14:25:36 +0800

    [repo] add static libraries from tongo

--- abi/get_emulator.go
+++ abi/get_emulator.go
@@ -1,5 +1,8 @@
 package abi
 
+// copied from tongo library
+// TODO: remove hardcoded libs
+
 // #cgo darwin LDFLAGS: -L ./lib/darwin/ -Wl,-rpath,./lib/darwin/ -l emulator
 // #cgo linux LDFLAGS: -L ./lib/linux/ -Wl,-rpath,./lib/linux/ -l emulator
 // #include "./lib/emulator-extern.h"
