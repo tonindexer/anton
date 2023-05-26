ccecd1bf66d1252f529958c86ed8d1bb7585da6a
iam047801 <datbooi@tuta.io>
2023-05-26 14:10:50 +0800

    [app] indexer: refactor to use fetcher service

--- internal/app/indexer.go
+++ internal/app/indexer.go
@@ -3,12 +3,19 @@ package app
 import (
 	"time"
 
+	"github.com/xssnick/tonutils-go/ton"
+
 	"github.com/tonindexer/anton/internal/core/repository"
 )
 
 type IndexerConfig struct {
-	DB               *repository.DB
-	Parser           ParserService
+	DB *repository.DB
+
+	API *ton.APIClient
+
+	Fetcher FetcherService
+	Parser  ParserService
+
 	FromBlock        uint32
 	FetchBlockPeriod time.Duration
 }
--- internal/app/indexer/tx.go
+++ internal/app/indexer/tx.go
@@ -7,106 +7,41 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/uptrace/bun"
-	"github.com/xssnick/tonutils-go/address"
-	"github.com/xssnick/tonutils-go/tlb"
 	"github.com/xssnick/tonutils-go/ton"
 
+	"github.com/tonindexer/anton/internal/app"
 	"github.com/tonindexer/anton/internal/core"
 )
 
-func (s *Service) fetchBlockTransactions(ctx context.Context, b *ton.BlockIDExt) ([]*tlb.Transaction, error) {
+func (s *Service) processBlockTransactions(ctx context.Context, tx bun.Tx, shard *ton.BlockIDExt) error {
 	var (
-		txID         *ton.TransactionID3
-		after        *ton.TransactionShortInfo
-		fetchedIDs   []ton.TransactionShortInfo
-		transactions []*tlb.Transaction
-		more         = true
-		err          error
+		accounts []*core.AccountState
+		messages []*core.Message
 	)
 
-	defer timeTrack(time.Now(), fmt.Sprintf("fetchBlockTransactions(%d, %d)", b.Workchain, b.SeqNo))
-
-	for more {
-		if after != nil {
-			txID = &ton.TransactionID3{
-				Account: after.Account,
-				LT:      after.LT,
-			}
-		}
-
-		fetchedIDs, more, err = s.api.GetBlockTransactionsV2(ctx, b, 100, txID)
-		if err != nil {
-			return nil, errors.Wrapf(err, "get b transactions (workchain = %d, seq = %d)",
-				b.Workchain, b.SeqNo)
-		}
-		if more {
-			after = &fetchedIDs[len(fetchedIDs)-1]
-		}
-
-		for _, id := range fetchedIDs {
-			addr := address.NewAddress(0, byte(b.Workchain), id.Account)
-
-			tx, err := s.api.GetTransaction(ctx, b, addr, id.LT)
-			if err != nil {
-				return nil, errors.Wrapf(err, "get transaction (workchain = %d, seq = %d, addr = %s, lt = %d)",
-					b.Workchain, b.SeqNo, addr.String(), id.LT)
-			}
-
-			transactions = append(transactions, tx)
-		}
-	}
-
-	return transactions, nil
-}
-
-func (s *Service) processBlockTransactions(ctx context.Context, tx bun.Tx, shard *ton.BlockIDExt) error {
-	var accounts []*core.AccountState
-	var accountData []*core.AccountData
-
-	blockTx, err := s.fetchBlockTransactions(ctx, shard)
+	blockTx, err := s.Fetcher.FetchBlockTransactions(ctx, shard)
 	if err != nil {
 		return errors.Wrap(err, "get block transactions")
 	}
 
-	transactions, err := mapTransactions(shard, blockTx)
-	if err != nil {
-		return errors.Wrap(err, "parse block transactions")
+	for i := range blockTx {
+		accounts = append(accounts, blockTx[i].Account)
 	}
 
-	accountMap, accountDataMap, err := s.processTxAccounts(ctx, shard, transactions)
-	if err != nil {
-		return errors.Wrap(err, "process tx accounts")
+	for i := range blockTx {
+		messages = append(messages, blockTx[i].InMsg)
+		messages = append(messages, blockTx[i].OutMsg...)
 	}
 
-	messages, err := s.processBlockMessages(ctx, tx, shard, blockTx)
-	if err != nil {
-		return errors.Wrap(err, "parse block messages")
-	}
-
-	payloads := s.parseMessagePayloads(ctx, tx, messages, accountDataMap)
-
-	for _, st := range accountMap {
-		accounts = append(accounts, st)
-	}
-	for _, st := range accountDataMap {
-		accountData = append(accountData, st)
-	}
-
-	defer timeTrack(time.Now(), fmt.Sprintf("add account and transactions data(%d, %d)", shard.Workchain, shard.SeqNo))
+	defer app.TimeTrack(time.Now(), fmt.Sprintf("add account and transactions data(%d, %d)", shard.Workchain, shard.SeqNo))
 
 	if err := s.accountRepo.AddAccountStates(ctx, tx, accounts); err != nil {
 		return errors.Wrap(err, "add account states")
 	}
-	if err := s.accountRepo.AddAccountData(ctx, tx, accountData); err != nil {
-		return errors.Wrap(err, "add account data")
-	}
-	if err := s.msgRepo.AddMessagePayloads(ctx, tx, payloads); err != nil {
-		return errors.Wrap(err, "add message payloads")
-	}
 	if err := s.msgRepo.AddMessages(ctx, tx, messages); err != nil {
 		return errors.Wrap(err, "add messages")
 	}
-	if err := s.txRepo.AddTransactions(ctx, tx, transactions); err != nil {
+	if err := s.txRepo.AddTransactions(ctx, tx, blockTx); err != nil {
 		return errors.Wrap(err, "add transactions")
 	}
 
