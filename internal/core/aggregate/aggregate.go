917c29be6c896474fcad6410dd9e000f19e58f65
iam047801 <datbooi@tuta.io>
2023-06-29 15:38:49 +0800

    [core] optimized statistics calculation

--- internal/core/aggregate/aggregate.go
+++ internal/core/aggregate/aggregate.go
@@ -2,6 +2,8 @@ package aggregate
 
 import (
 	"context"
+	"sort"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/uptrace/bun"
@@ -10,6 +12,16 @@ import (
 	"github.com/tonindexer/anton/abi"
 	"github.com/tonindexer/anton/internal/core"
 )
+
+type AccountStatusCount struct {
+	Status core.AccountStatus `json:"status"`
+	Count  int                `json:"count"`
+}
+
+type AccountTypesCount struct {
+	Interfaces []abi.ContractName `json:"interfaces"`
+	Count      int                `json:"count"`
+}
 
 type Statistics struct {
 	FirstBlock       int `json:"first_masterchain_block"`
@@ -30,15 +42,9 @@ type Statistics struct {
 	ContractInterfaceCount int `json:"contract_interface_count"`
 	ContractOperationCount int `json:"contract_operation_count"`
 
-	AccountStatusCount []struct {
-		Status core.AccountStatus `json:"status"`
-		Count  int                `json:"count"`
-	} `json:"account_count_by_status"`
+	AccountStatusCount []AccountStatusCount `json:"account_count_by_status"`
 
-	AccountTypesCount []struct {
-		Interfaces []abi.ContractName `json:"interfaces"`
-		Count      int                `json:"count"`
-	} `json:"account_count_by_interfaces"`
+	AccountTypesCount []AccountTypesCount `json:"account_count_by_interfaces"`
 
 	MessageTypesCount []struct {
 		Operation string `json:"operation"`
@@ -46,9 +52,35 @@ type Statistics struct {
 	} `json:"message_count_by_operation"`
 }
 
-func GetStatistics(ctx context.Context, ck *ch.DB, pg *bun.DB) (*Statistics, error) {
-	var ret Statistics
+type accountCount struct {
+	Parsed       uint8
+	AccountCount int
+}
 
+type addressCount struct {
+	Status     core.AccountStatus
+	Interfaces []abi.ContractName
+	Count      int
+}
+
+func marshalInterfacesKey(interfaces []abi.ContractName) string {
+	var str []string
+	for _, i := range interfaces {
+		str = append(str, string(i))
+	}
+	sort.Strings(str)
+	return strings.Join(str, ",")
+}
+
+func unmarshalInterfacesKey(raw string) (ret []abi.ContractName) {
+	str := strings.Split(raw, ",")
+	for _, s := range str {
+		ret = append(ret, abi.ContractName(s))
+	}
+	return ret
+}
+
+func getBlockStatistics(ctx context.Context, ck *ch.DB, ret *Statistics) error {
 	err := ck.NewSelect().Model((*core.Block)(nil)).
 		ColumnExpr("count(seq_no) as masterchain_block_count").
 		ColumnExpr("min(seq_no) as first_masterchain_block").
@@ -56,51 +88,66 @@ func GetStatistics(ctx context.Context, ck *ch.DB, pg *bun.DB) (*Statistics, err
 		Where("workchain = -1").
 		Scan(ctx, &ret.MasterBlockCount, &ret.FirstBlock, &ret.LastBlock)
 	if err != nil {
-		return nil, errors.Wrap(err, "first and last masterchain blocks")
+		return errors.Wrap(err, "first and last masterchain blocks")
+	}
+	return nil
+}
+
+func getAccountStatistics(ctx context.Context, ck *ch.DB, ret *Statistics) error {
+	var accounts []accountCount
+
+	err := ck.NewSelect().Model((*core.AccountState)(nil)).
+		ColumnExpr("length(types) > 0 as parsed").
+		ColumnExpr("count(address) as account_count").
+		Group("parsed").
+		Scan(ctx, &accounts)
+	if err != nil {
+		return errors.Wrap(err, "account state count")
+	}
+	for _, r := range accounts {
+		ret.AccountCount += r.AccountCount
+		if r.Parsed > 0 {
+			ret.ParsedAccountCount += r.AccountCount
+		}
 	}
 
-	ret.AccountCount, err = ck.NewSelect().Model((*core.AccountState)(nil)).Count(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "account state count")
-	}
-	ret.ParsedAccountCount, err = ck.NewSelect().Model((*core.AccountState)(nil)).Where("length(types) > 0").Count(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "account data count")
-	}
-
+	var addresses []addressCount
 	err = ck.NewSelect().
 		ColumnExpr("last_status as status").
-		ColumnExpr("count(addr) as count").
-		TableExpr("(?) as q",
-			ck.NewSelect().
-				Model((*core.AccountState)(nil)).
-				ColumnExpr("argMax(address, last_tx_lt) as addr").
-				ColumnExpr("argMax(status, last_tx_lt) as last_status").
-				Group("address")).
-		Group("last_status").
-		Order("count DESC").
-		Scan(ctx, &ret.AccountStatusCount)
-	if err != nil {
-		return nil, errors.Wrap(err, "account status count")
-	}
-	for _, row := range ret.AccountStatusCount {
-		ret.AddressCount += row.Count
-	}
-
-	err = ck.NewSelect().
 		ColumnExpr("last_types as interfaces").
 		ColumnExpr("count(addr) as count").
 		TableExpr("(?) as q",
 			ck.NewSelect().
 				Model((*core.AccountState)(nil)).
 				ColumnExpr("argMax(address, last_tx_lt) as addr").
+				ColumnExpr("argMax(status, last_tx_lt) as last_status").
 				ColumnExpr("argMax(types, last_tx_lt) as last_types").
 				Group("address")).
-		Group("last_types").
-		Order("count DESC").
-		Scan(ctx, &ret.AccountTypesCount)
+		Group("last_status", "last_types").
+		Scan(ctx, &addresses)
 	if err != nil {
-		return nil, errors.Wrap(err, "account interfaces count")
+		return errors.Wrap(err, "address count")
+	}
+
+	accountStatuses := map[core.AccountStatus]int{}
+	accountTypes := map[string]int{}
+	for _, r := range addresses {
+		accountStatuses[r.Status] += r.Count
+		accountTypes[marshalInterfacesKey(r.Interfaces)] += r.Count
+	}
+
+	for s, c := range accountStatuses {
+		ret.AccountStatusCount = append(ret.AccountStatusCount, AccountStatusCount{Status: s, Count: c})
+	}
+	for i, c := range accountTypes {
+		ret.AccountTypesCount = append(ret.AccountTypesCount, AccountTypesCount{Interfaces: unmarshalInterfacesKey(i), Count: c})
+	}
+
+	sort.Slice(ret.AccountStatusCount, func(i, j int) bool { return ret.AccountStatusCount[i].Count < ret.AccountStatusCount[j].Count })
+	sort.Slice(ret.AccountTypesCount, func(i, j int) bool { return ret.AccountTypesCount[i].Count < ret.AccountTypesCount[j].Count })
+
+	for _, row := range ret.AccountStatusCount {
+		ret.AddressCount += row.Count
 	}
 	for _, row := range ret.AccountTypesCount {
 		if len(row.Interfaces) == 0 {
@@ -109,26 +156,59 @@ func GetStatistics(ctx context.Context, ck *ch.DB, pg *bun.DB) (*Statistics, err
 		ret.ParsedAddressCount += row.Count
 	}
 
-	ret.TransactionCount, err = ck.NewSelect().Model((*core.Transaction)(nil)).Count(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "transaction count")
-	}
-	ret.MessageCount, err = ck.NewSelect().Model((*core.Message)(nil)).Count(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "message count")
-	}
-	err = ck.NewSelect().Model((*core.Message)(nil)).
+	return nil
+}
+
+func getTransactionStatistics(ctx context.Context, ck *ch.DB, ret *Statistics) error {
+	err := ck.NewSelect().Model((*core.Message)(nil)).
 		ColumnExpr("operation_name as operation").
 		ColumnExpr("count() as count").
-		Where("length(operation_name) > 0").
 		Group("operation_name").
 		Order("count DESC").
 		Scan(ctx, &ret.MessageTypesCount)
 	if err != nil {
-		return nil, errors.Wrap(err, "account interfaces count")
+		return errors.Wrap(err, "message types count")
 	}
-	for _, row := range ret.MessageTypesCount {
-		ret.ParsedMessageCount += row.Count
+
+	unknownOp := -1
+	for it, row := range ret.MessageTypesCount {
+		ret.MessageCount += row.Count
+		if row.Operation != "" {
+			ret.ParsedMessageCount += row.Count
+		} else {
+			unknownOp = it
+		}
+	}
+	if unknownOp == len(ret.MessageTypesCount)-1 {
+		ret.MessageTypesCount = ret.MessageTypesCount[:unknownOp]
+	} else if unknownOp != -1 {
+		ret.MessageTypesCount = append(ret.MessageTypesCount[:unknownOp], ret.MessageTypesCount[unknownOp+1:]...)
+	}
+
+	ret.TransactionCount, err = ck.NewSelect().Model((*core.Transaction)(nil)).Count(ctx)
+	if err != nil {
+		return errors.Wrap(err, "transaction count")
+	}
+
+	return nil
+}
+
+func GetStatistics(ctx context.Context, ck *ch.DB, pg *bun.DB) (*Statistics, error) {
+	var (
+		ret Statistics
+		err error
+	)
+
+	if err := getBlockStatistics(ctx, ck, &ret); err != nil {
+		return nil, err
+	}
+
+	if err := getAccountStatistics(ctx, ck, &ret); err != nil {
+		return nil, err
+	}
+
+	if err := getTransactionStatistics(ctx, ck, &ret); err != nil {
+		return nil, err
 	}
 
 	ret.ContractInterfaceCount, err = pg.NewSelect().Model((*core.ContractInterface)(nil)).Count(ctx)
