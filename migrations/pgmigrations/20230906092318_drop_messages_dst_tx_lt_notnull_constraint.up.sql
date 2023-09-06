3f64ce5e8b8d16aab2871a2722705f2cb075f9ee
iam047801 <datbooi@tuta.io>
2023-09-06 18:55:56 +0800

    remove extra space ...

--- migrations/pgmigrations/20230906092318_drop_messages_dst_tx_lt_notnull_constraint.up.sql
+++ migrations/pgmigrations/20230906092318_drop_messages_dst_tx_lt_notnull_constraint.up.sql
@@ -11,7 +11,7 @@ ALTER TABLE messages ADD CONSTRAINT messages_tx_lt_notnull CHECK ((
         (type = 'EXTERNAL_OUT') AND (src_address IS NOT NULL) AND (src_tx_lt IS NOT NULL) AND (dst_address IS NULL) AND (dst_tx_lt IS NULL)
     ) OR (
         (type = 'EXTERNAL_IN') AND (src_address IS NULL) AND (src_tx_lt IS NULL) AND (dst_address IS NOT NULL) AND (dst_tx_lt IS NOT NULL)
-    )  OR (
+    ) OR (
         (type = 'INTERNAL') AND (src_workchain != -1 OR dst_workchain != -1) AND (src_tx_lt IS NOT NULL)
     ) OR (
         (type = 'INTERNAL') AND src_workchain = -1 AND dst_workchain = -1
