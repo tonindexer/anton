0c51beb44fdff3deadaf5bc841ff1d52048c103a
iam047801 <datbooi@tuta.io>
2024-03-12 22:00:05 +0700

    [migrations] update rescan table

--- migrations/pgmigrations/20240213085742_reindex_state.down.sql
+++ migrations/pgmigrations/20240213085742_reindex_state.down.sql
@@ -1,5 +1,7 @@
 SET statement_timeout = 0;
 
-DROP TABLE rescan_tasks;
+BEGIN;
+    DROP TABLE rescan_tasks;
 
-DROP INDEX account_states_workchain_shard_block_seq_no_idx;
+    DROP TYPE rescan_task_type;
+COMMIT;
--- migrations/pgmigrations/20240213085742_reindex_state.up.sql
+++ migrations/pgmigrations/20240213085742_reindex_state.up.sql
@@ -1,22 +1,37 @@
 SET statement_timeout = 0;
 
 BEGIN;
+    CREATE TYPE rescan_task_type AS ENUM (
+        'add_interface',
+        'upd_interface',
+        'del_interface',
+        'add_get_method',
+        'del_get_method',
+        'upd_get_method',
+        'upd_operation',
+        'del_operation'
+    );
+
     CREATE SEQUENCE rescan_tasks_id_seq START WITH 1;
 
     CREATE TABLE rescan_tasks (
         id integer NOT NULL DEFAULT nextval('rescan_tasks_id_seq'),
         finished bool NOT NULL,
-        start_from_masterchain_seq_no integer NOT NULL,
-        accounts_last_masterchain_seq_no integer NOT NULL,
-        accounts_rescan_done boolean NOT NULL,
-        messages_last_masterchain_seq_no integer NOT NULL,
-        messages_rescan_done boolean NOT NULL,
+        type rescan_task_type NOT NULL,
+
+        contract_name text NOT NULL,
+
+        changed_get_method text NOT NULL,
+
+        message_type message_type NOT NULL,
+        outgoing boolean NOT NULL,
+        operation_id integer NOT NULL,
+
+        last_address bytea NOT NULL,
+        last_tx_lt bigint NOT NULL,
+
         CONSTRAINT rescan_tasks_pkey PRIMARY KEY (id)
     );
 
     ALTER SEQUENCE rescan_tasks_id_seq OWNED BY rescan_tasks.id;
-
-    CREATE UNIQUE INDEX ON rescan_tasks (finished) WHERE finished = false;
 COMMIT;
-
-CREATE INDEX account_states_workchain_shard_block_seq_no_idx ON account_states USING btree (workchain, shard, block_seq_no);
