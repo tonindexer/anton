7f74263ce07c8091e9c6ea489d560f0a1bce726e
iam047801 <datbooi@tuta.io>
2025-06-26 13:17:55 +0300

    [repo] tx filter count: round max lt

--- internal/core/repository/tx/filter.go
+++ internal/core/repository/tx/filter.go
@@ -78,14 +78,13 @@ func (r *Repository) filterTx(ctx context.Context, req *filter.TransactionsReq) 
 
 func (r *Repository) countTxFullScan(ctx context.Context, req *filter.TransactionsReq) (count int, maxLt uint64, err error) {
 	var result struct {
-		Count int
-		MaxLT *uint64 `ch:"max_lt"`
+		Count        int
+		MaxLT        uint64 `ch:"max_lt_value"`
+		RoundedMaxLT uint64 `ch:"max_lt_rounded"`
 	}
 
 	q := r.ch.NewSelect().
-		Model((*core.Transaction)(nil)).
-		ColumnExpr("count(*) AS count").
-		ColumnExpr("(SELECT max(created_lt) FROM transactions) AS max_lt") // unfiltered max
+		Model((*core.Transaction)(nil))
 
 	if len(req.Hash) > 0 {
 		q = q.Where("hash = ?", req.Hash)
@@ -108,43 +107,86 @@ func (r *Repository) countTxFullScan(ctx context.Context, req *filter.Transactio
 		q = q.Where("created_lt = ?", *req.CreatedLT)
 	}
 
-	if err := q.Scan(ctx, &result); err != nil {
-		return 0, 0, err
-	}
-
-	if result.MaxLT == nil {
-		return 0, 0, core.ErrNotFound
-	}
-
-	return result.Count, *result.MaxLT, nil
-}
-
-func (r *Repository) countTxPartialScan(ctx context.Context, req *filter.TransactionsReq, startLt uint64) (partialCount int, maxLt uint64, err error) {
-	var result struct {
-		Count int
-		MaxLT uint64 `bun:"max_lt"`
-	}
-
-	q := r.pg.NewSelect().
-		Model((*core.Transaction)(nil)).
-		ColumnExpr("count(*) AS count").
-		ColumnExpr("COALESCE(max(created_lt), ?) AS max_lt", startLt).
-		Where("created_lt > ?", startLt)
-
-	q = r.getFilterTxQuery(q, &req.TransactionsFilter)
+	q = r.ch.NewSelect().
+		With(
+			"max_lt",
+			r.ch.NewSelect().
+				Model((*core.Transaction)(nil)).
+				ColumnExpr("max(created_lt) AS v"),
+		).
+		With(
+			"rounded_count",
+			q. // query with filters
+				Table("max_lt").
+				ColumnExpr("count(*) as v").
+				Where("created_lt <= floor(max_lt.v, -7) - 1e7"),
+		).
+		Table("max_lt", "rounded_count").
+		ColumnExpr("max_lt.v AS max_lt_value").
+		ColumnExpr("max2(floor(max_lt.v, -7) - 1e7, 0) as max_lt_rounded").
+		ColumnExpr("rounded_count.v AS count")
 
 	if err := q.Scan(ctx, &result); err != nil {
 		return 0, 0, err
 	}
 
 	if result.MaxLT == 0 {
-		result.MaxLT = startLt // no new rows
+		return 0, 0, core.ErrNotFound
 	}
 
-	return result.Count, result.MaxLT, nil
+	return result.Count, result.RoundedMaxLT, nil
+}
+
+func (r *Repository) countTxPartialScan(ctx context.Context, req *filter.TransactionsReq, startLt uint64) (partialCount, roundedCount int, roundedMaxLt uint64, err error) {
+	var result struct {
+		SinceStartCount int    `bun:"since_start_count"`
+		RoundedCount    int    `bun:"until_rounded_count"`
+		RoundedMaxLT    uint64 `bun:"rounded_max_lt_value"`
+	}
+
+	q := r.pg.NewSelect().
+		With(
+			"rounded_max_lt",
+			r.pg.NewSelect().
+				Model((*core.Transaction)(nil)).
+				ColumnExpr("greatest(floor(max(created_lt) / 1e7) * 1e7 - 1e7, 0) AS v"), // we round LT as transactions in new blocks can have lower LT
+		).
+		With(
+			"until_rounded_count",
+			r.getFilterTxQuery(
+				r.pg.NewSelect().Model((*core.Transaction)(nil)),
+				&req.TransactionsFilter,
+			).
+				Table("rounded_max_lt").
+				ColumnExpr("count(*) as v").
+				Where("created_lt > ?", startLt).
+				Where("created_lt <= rounded_max_lt.v"),
+		).
+		With("since_start_count",
+			r.getFilterTxQuery(
+				r.pg.NewSelect().Model((*core.Transaction)(nil)),
+				&req.TransactionsFilter,
+			).
+				ColumnExpr("count(*) as v").
+				Where("created_lt > ?", startLt)).
+		Table("rounded_max_lt", "until_rounded_count", "since_start_count").
+		ColumnExpr("since_start_count.v AS since_start_count").
+		ColumnExpr("until_rounded_count.v AS until_rounded_count").
+		ColumnExpr("rounded_max_lt.v as rounded_max_lt_value")
+
+	if err := q.Scan(ctx, &result); err != nil {
+		return 0, 0, 0, err
+	}
+
+	return result.SinceStartCount, result.RoundedCount, result.RoundedMaxLT, nil
 }
 
 func (r *Repository) countTx(ctx context.Context, req *filter.TransactionsReq) (int, error) {
+	if len(req.Hash) > 0 || req.BlockID != nil || req.CreatedLT != nil { // count value cannot change on any of these filters
+		count, _, _, err := r.countTxPartialScan(ctx, req, 0)
+		return count, err
+	}
+
 	count, maxLT, err := r.transactionsFilterCountCache.Get(req.TransactionsFilter)
 	if errors.Is(err, core.ErrNotFound) {
 		count, maxLT, err = r.countTxFullScan(ctx, req)
@@ -161,15 +203,11 @@ func (r *Repository) countTx(ctx context.Context, req *filter.TransactionsReq) (
 		return 0, err
 	}
 
-	if len(req.Hash) > 0 || req.BlockID != nil || req.CreatedLT != nil {
-		return count, nil // count value cannot change on any of these filters
-	}
-
-	partialCount, maxLT, err := r.countTxPartialScan(ctx, req, maxLT)
+	partialCount, roundedPartialCount, roundedMaxLT, err := r.countTxPartialScan(ctx, req, maxLT)
 	if err != nil {
 		return 0, err
 	}
-	if err := r.transactionsFilterCountCache.Set(req.TransactionsFilter, count+partialCount, maxLT); err != nil {
+	if err := r.transactionsFilterCountCache.Set(req.TransactionsFilter, count+roundedPartialCount, roundedMaxLT); err != nil {
 		return 0, err
 	}
 
